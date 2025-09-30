676397328100c4fdab78a288901968b0d4abaad7
John Hillan <j.hillan@wlv.ac.uk>
2025-09-30 02:34:44 +0100

    Activity 2: Usable Mobile Forms

--- mobile_hci.js
+++ mobile_hci.js
@@ -1,6 +1,93 @@
 // Module 7CS069 Web Technologies
 // Week 3 - Mobile HCI
 
+// Form component for entering username/password
+
+class UsernamePasswordForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {userName: '', password: '', rememberUsername: false};
+    this.handleUserNameChange = this.handleUserNameChange.bind(this);
+    this.handlePasswordChange = this.handlePasswordChange.bind(this);
+    this.handleRememberUsernameChange = this.handleRememberUsernameChange.bind(this);
+    this.handleSubmitChange = this.handleSubmitChange.bind(this);
+  }
+
+  handleUserNameChange(userNameEvent) {
+    const userName = userNameEvent.target.value;
+    console.log("handleUsernameChange " + userName);
+    this.setState(
+      {userName: userName,
+       password: this.state.password,
+       rememberUsername: this.state.rememberUsername});
+  }
+
+  handlePasswordChange(passwordEvent) {
+    const password = passwordEvent.target.value;
+    console.log("handlePasswordChange " + password);
+    this.setState(
+      {userName: this.state.userName,
+       password: password,
+       rememberUsername: this.state.rememberUsername});
+  }
+
+  handleRememberUsernameChange(rememberUsernameEvent) {
+    const rememberUsername = rememberUsernameEvent.target.checked;
+    console.log("handleRememberUsernameChange " + rememberUsername);
+    this.setState(
+      {userName: this.state.userName,
+       password: this.state.password,
+       rememberUsername: rememberUsername});
+  }
+
+  handleSubmitChange(submit) {
+    console.log("handleSubmitChange '" + this.state.userName + "'");
+    this.props.onSubmitChange(
+      this.state.userName,
+      this.state.password,
+      this.state.rememberUsername);
+  }
+
+  render() {
+    return (
+      <form>
+        <div className="mb-3">
+          <label className="form-label" formAction="handleUsernameChange">
+            Email address
+          </label>
+          <input
+            type="email" className="form-control" id="userName"
+            aria-describedby="emailHelp"
+            value={this.props.username}
+            onChange={this.handleUserNameChange} />
+          <div id="emailHelp" className="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div className="mb-3">
+          <label className="form-label">Password</label>
+          <input
+            type="password" className="form-control" id="password"
+            value={this.props.password}
+            onChange={this.handlePasswordChange} />
+        </div>
+        <div className="mb-3 form-check">
+          <label className="form-check-label">Remember username</label>
+          <input
+            type="checkbox" className="form-check-input" id="rememberUsername"
+            checked={this.props.rememberUsername}
+            onChange={this.handleRememberUsernameChange} />
+        </div>
+        <button
+          type="submit" className="btn btn-primary"
+          onClick={this.handleSubmitChange}>
+          Submit
+        </button>
+      </form>
+    );
+  }
+}
+
 // Grid component
 // - column is col-<screen size>-<number out of 12 to use>
 // - screen size is
@@ -12,10 +99,15 @@
 class GridComponent extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-      columnText1: props.columnText1,
-      columnText2: props.columnText2,
-      columnText3: props.columnText3};
+    // Handler for login form submit button change
+    this.handleLoginFormSubmitChange = this.handleLoginFormSubmitChange.bind(this);
+  }
+
+  handleLoginFormSubmitChange(userName, password, rememberUsername) {
+    alert("Login form submitted" +
+          "\n  user name '" + userName + "'" +
+          "\n  password  '" + password + "'" +
+          "\n  remember  "  + rememberUsername);
   }
 
   render() {
@@ -23,13 +115,14 @@ class GridComponent extends React.Component {
       <div className="container text-center position-relative top-0 start-25">
         <div className="row">
           <div className="col-xs-4 col-sm-3 col-md-2 col-lg-2 p-3 align-middle align-self-center">
-            {this.state.columnText1}
+            Welcome to John Hillan's William Shakespeare Page
           </div>
           <div className="col-xs-4 col-sm-4 col-md-4 col-lg-3 p-3 align-middle align-self-center">
-            {this.state.columnText2}
+            <img src="Shakespeare.jpg" className="img-thumbnail" />
           </div>
           <div className="col-xs-4 col-sm-5 col-md-6 col-lg-7 p-3 align-middle align-self-center">
-            {this.state.columnText3}
+            <UsernamePasswordForm
+              onSubmitChange={this.handleLoginFormSubmitChange} />
           </div>
         </div>
       </div>
@@ -41,17 +134,10 @@ class GridComponent extends React.Component {
 
 function App() {
 
-  const columnText1 = "This is the content for column 1 which is relatively brief";
-  const columnText2 = "This is the content for column 2 which will normally be slightly longer than the left hand column";
-  const columnText3 = "This is the content for column 3 which is meant to be where much longer pieces of text such as full reviews can be placed as opposed to the first two fields which could be an index and a summary";
-
   return (
     <div className="App">
       <br />
-      <GridComponent
-        columnText1={columnText1}
-        columnText2={columnText2}
-        columnText3={columnText3} />
+      <GridComponent />
     </div>
   );
 }
